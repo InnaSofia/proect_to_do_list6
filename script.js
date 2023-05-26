12f12d509f7990ff2ba8d2a29aebdfb9b4f4c3af
InnaSofia <inna.artemieva.mironova@mail.ru>
2023-05-26 13:05:30 +0300

    кнопка

---
+++ script.js
@@ -0,0 +1,21 @@
+
+let count = 0
+function makeTest(){
+    let conteiner = document.getElementById("conteiner")
+    let myTitle = document.getElementById("title")
+    let input = document.getElementById("input")  
+    let myValue = input.value
+    count = count + 1
+    conteiner.innerHTML = conteiner.innerHTML + count + input.value
+}
+
+const input = document.getElementById("input")
+const task = input.value
+
+let username = prompt("Введите своё имя:")
+if (username){
+    document.getElementById('title').innerHTML = "Список дел," +" " + username + "," + ":" ;
+} else{
+    document.getElementById('title').innerHTML = "Список дел ананимного пользователя!";  
+}
+
