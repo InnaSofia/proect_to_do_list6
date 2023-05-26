51c7b8411729ff7222796e5ae57217c7ee1d8811
InnaSofia <inna.artemieva.mironova@mail.ru>
2023-05-26 17:47:51 +0300

    css

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
+let username = prompt("Введите своё имя:")
+let myTitle = document.getElementById("title")
+
+
+if (username){
+    myTitle.innerHTML = "Список дел," +" " + username + "," + ":" ;
+} else{
+    myTitle.innerHTML = "Список дел ананимного пользователя!";  
+}
+
 
 let count = 0
 function makeTest(){
-    let conteiner = document.getElementById("conteiner")
-    let myTitle = document.getElementById("title")
-    let input = document.getElementById("input")  
-    let value = input.value
+    let conteiner = document.getElementById("conteiner")//здесь дела
+   
+    let input = document.getElementById("input")//инпут  
+    let value = input.value//значение инпут
     count = count + 1
-    conteiner.innerHTML = conteiner.innerHTML + count + input.value
 
-    
-        
-
-        if(count > 5){
-            document.getElementById('text2').innerHTML = "Можно ввести до 5 дел"
-        }
+    if(count <= 5){
+        conteiner.innerHTML = conteiner.innerHTML + count + value
+    }else if(count > 5){
+alert ("Можно ввести до 5 дел")
     }
-
-
-const input = document.getElementById("input")
-const text2 = input.value
-
-let username = prompt("Введите своё имя:")
-if (username){
-    document.getElementById('title').innerHTML = "Список дел," +" " + username + "," + ":" ;
-} else{
-    document.getElementById('title').innerHTML = "Список дел ананимного пользователя!";  
 }
-
